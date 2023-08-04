14015447b9c90943cdfe1dc9de8adad7937555b7
i-roger <mroger.dev@gmail.com>
2023-08-04 15:22:51 -0300

    Projects section

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,13 @@
 import SobreMim from '../sections/sobremim'
 import Habilidades from '../sections/habilidades'
+import Projetos from '../sections/projetos'
 
 export default function Home() {
     return(
         <>
         <SobreMim />
         <Habilidades />
+        <Projetos />
         </>
     )
 }
